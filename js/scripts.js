e3e90a68556652e3d12aeccf7de1f1173fc985b0
Bryce Frazier and Sara Hamilton <sara@>
2018-01-17 16:36:06 -0800

    modify jQuery

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var isPalindrome = function(word) {
+var wordArray = word.split(""); // turn string in array
+reversedArray = wordArray.reverse(); // make new array with reverse
+reversedString = reversedArray.join(""); // make string of reversed array
+if (word === reversedString){ // compare the two strings
+  return true // return true or false
+} else {
+  return false
+}
+}
+
+$(document).ready(function(){
+  $("#palindromeForm").submit(function(event){
+    event.preventDefault();
+    var word = $("input#word").val();
+    if (isPalindrome(word)) {
+      $(".yes").text(word + " is ");
+    } else {
+      $(".no").text(word + " is not ");
+    }
+    $("#response").show();
+  });
+});
